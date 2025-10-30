b84c063f292aaa5bdb336d5492a060dabf74f7a3
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-10-30 14:42:09 +0800

    update enums

--- Lababa/Backend/Models/ServicePricing.cs
+++ Lababa/Backend/Models/ServicePricing.cs
@@ -8,4 +8,12 @@
         public decimal UnitPrice { get; set; }
         public int OrderId { get; set; }
     }
+    public class WeightServiceItem : IEntity
+    {
+        public int Id { get; set; }
+        public string WashType { get; set; }
+        public decimal PricePerUnit { get; set; }
+        public double Weight { get; set; }
+        public int OrderId { get; set; }
+    }
 }
