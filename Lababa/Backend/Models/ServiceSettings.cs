1adec07af3700853fca84f082337a6af5a0d5b03
Van-Excel-Arroz <vanexcelarroz@gmail.com>
2025-11-05 08:22:34 +0800

    moved some classes to its own file

--- Lababa/Backend/Models/ServiceSettings.cs
+++ Lababa/Backend/Models/ServiceSettings.cs
@@ -2,21 +2,6 @@
 
 namespace Lababa.Backend.Models
 {
-    public class ShopSettings
-    {
-        public string ShopName { get; set; }
-        public string Address { get; set; }
-        public string PhoneNumber { get; set; }
-        public string CurrencySymbol { get; set; }
-        public WeightUnit DefaultWeightUnit { get; set; }
-    }
-
-    public class ReceiptSettings
-    {
-        public string HeaderMessage { get; set; }
-        public string FooterMessage { get; set; }
-    }
-
     public class WeigthServiceCatalogEntry : IEntity
     {
         public Guid Id { get; set; }
---
+++ Lababa/Backend/Models/ShopSettings.cs
@@ -0,0 +1,17 @@
+﻿namespace Lababa.Backend.Models
+{
+    public class ShopSettings
+    {
+        public string ShopName { get; set; }
+        public string Address { get; set; }
+        public string PhoneNumber { get; set; }
+        public string CurrencySymbol { get; set; }
+        public WeightUnit DefaultWeightUnit { get; set; }
+    }
+
+    public class ReceiptSettings
+    {
+        public string HeaderMessage { get; set; }
+        public string FooterMessage { get; set; }
+    }
+}
